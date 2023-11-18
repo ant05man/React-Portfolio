2b70069a7769bef0106f0278ea2c6f2f9c119e18
Anthony Littlejohn <ant05man@yahoo.com>
2023-11-18 11:22:18 -0500

    connected css to jsx files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import '../styles/Header.css';
-
+import React from 'react';
 // By importing the Header.css file, it is added to the DOM whenever this component loads
 
 // We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
@@ -7,21 +7,12 @@ import '../styles/Header.css';
 // On a style object, we camelCase all property names, and put all of the values in quotes
 // Non quoted values default to "pixels", e.g. height, margin, padding
 
-const styles = {
-  headerStyle: {
-    background: 'red',
-  },
-  headingStyle: {
-    fontSize: '100px',
-  },
-};
-
 // We use JSX curly braces to evaluate the style object
 
 function Header() {
   return (
-    <header style={styles.headerStyle} className="header">
-      <h1 style={styles.headingStyle}>Welcome</h1>
+    <header className="header">
+      <h1>Welcome</h1>
     </header>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,19 @@
 import '../styles/Navbar.css';
-
-// By importing the Navbar.css file, it is added to the DOM whenever this component loads
+import React from 'react';
+// By importing the Header.css file, it is added to the DOM whenever this component loads
 
 // We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
 // Unlike regular HTML, a JSX style property must be an object instead of a string
 // On a style object, we camelCase all property names, and put all of the values in quotes
 // Non quoted values default to "pixels", e.g. height, margin, padding
 
-const styles = {
-  navbarStyle: {
-    background: 'green',
-    justifyContent: 'flex-end',
-  },
-};
-
-// We use JSX curly braces to evaluate the style object on the JSX tag
+// We use JSX curly braces to evaluate the style object
 
 function Navbar() {
   return (
-    <nav style={styles.navbarStyle} className="navbar">
-      <a href="/">Welcome</a>
-    </nav>
+    <header className="navbar">
+      <h1>Welcome</h1>
+    </header>
   );
 }
 
