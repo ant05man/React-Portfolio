d33d956f889110fc99ed829bd6ef393fd5bcc581
Anthony Littlejohn <ant05man@yahoo.com>
2023-11-18 10:54:28 -0500

    changed index.jsx code

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,7 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
