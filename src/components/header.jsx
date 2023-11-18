1cbcb9a102d348cf103fdaef697108295d3224dd
Anthony Littlejohn <ant05man@yahoo.com>
2023-11-18 10:04:56 -0500

    added files to components folder

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+// Here we are importing a CSS file as a dependency
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Home</h1>
+    </header>
+  );
+}
+
+export default Header;
