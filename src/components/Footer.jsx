43ddd1dec4370e1d612535d357f6dc8eb0d2342b
Anthony Littlejohn <ant05man@yahoo.com>
2023-11-19 17:44:45 -0500

    fixed LinkedIn link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <div className="footer" id="footer">
                 <ul>
                 <li><a href="https://www.github.com/ant05man">GitHub</a></li>
-                <li><a href="linkedin.com/in/anthony-l-b767791b7/">LinkedIn</a></li>
+                <li><a href="https://www.linkedin.com/in/anthony-l-b767791b7/">LinkedIn</a></li>
                 </ul>
         </div>
     )
