bc660a3ad2f9be87c559ff33195a2ac61c3cd6fb
Anthony Littlejohn <ant05man@yahoo.com>
2024-01-08 18:38:18 -0500

    changed route to screenshots

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -14,34 +14,34 @@ function Portfolio() {
 
                 <div id="box2" className="imageContainer">
                     <a href="https://ant05man.github.io/Landing-Page-Project/">
-                        <img src="src/components/Screenshot (5).png" />
+                        <img src="/Screenshot (5).png" />
                     </a>
                     <div className="bottom-center">Landing Page Mini-Project</div>
                 </div>
 
                 <div id="box3" className="imageContainer">
                     <a href="https://ant05man.github.io/Flexbox-Mini-Project/">
-                        <img src="src/components/Screenshot (6).png" />
+                        <img src="/Screenshot (6).png" />
                     </a>
                     <div className="bottom-center">Flexbox Mini Project</div>
                 </div>
 
                 <div id="box4" className="imageContainer">
                     <a href="https://briellebroadt.github.io/Witty-Web-Wizard/">
-                        <img src="src/components/Screenshot (22).png" />
+                        <img src="/Screenshot (22).png" />
                     </a>
                     <div className="bottom-center">Witty Web Wizard</div>
                 </div>
 
                 <div id="box5" className="imageContainer">
                     <a href="https://ant05man.github.io/Workday-scheduler/">
-                        <img src="src/components/Screenshot (42).png" />
+                        <img src="/Screenshot (42).png" />
                     </a>
                     <div className="bottom-center">Workday-Scheduler Project</div>
                 </div>
                 <div id="box6" className="imageContainer">
                     <a href="https://ant05man.github.io/password-generator/">
-                        <img src="src/components/Screenshot (8).png" />
+                        <img src="/Screenshot (8).png" />
                     </a>
                     <div className="bottom-center">PassWord Generator</div>
                 </div>
