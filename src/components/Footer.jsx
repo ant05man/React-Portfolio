eb841a3cd101dd3e28e8d7729d499567d919b0ac
Anthony Littlejohn <ant05man@yahoo.com>
2024-01-07 20:23:53 -0500

    created alt attribute for img

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ import '../styles/Footer.css';
 function Footer() {
     return (
         <div className="footer" id="footer">
-                <ul>
-                <li><a href="https://www.github.com/ant05man"><img src="/assets/github-mark-black.png" /></a></li>
+            <ul>
+                <li><a href="https://www.github.com/ant05man"><img src="/assets/github-mark-black.png" alt="GitHub" /></a></li>
                 <li><a href="https://www.linkedin.com/in/anthony-l-b767791b7/">LinkedIn</a></li>
-                </ul>
+            </ul>
         </div>
     )
 }
