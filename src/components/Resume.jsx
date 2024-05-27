9225dd97ae3811c99e3353b914a9b5d538671046
Anthony Littlejohn <ant05man@yahoo.com>
2024-05-27 18:10:07 -0400

    changed colors of some font in resume page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,47 +1,47 @@
+// Resume.js
 import React from 'react';
-import '../styles/Resume.css/';
+import '../styles/Resume.css';
 
 const Resume = () => {
   return (
     <div className="resumeDiv">
       <h2 className="resumeTitle">Resume</h2>
-       {/* Download link */}
-       <a href="/src/components/AnthonyL Resume.docx" className="resumeDownload" download>
-        Download Resume<br></br><br></br>
+      {/* Download link */}
+      <a href="/src/components/AnthonyL Resume.docx" className="resumeDownload" download>
+        Download Resume<br /><br />
       </a>
-      <ul>
-      <h3 className="resumeInfo">
-        <li className="resumeTitle">Education</li>
-        <li>
-            <span style={{ color: 'red' }}>
-            <img src="/rutgers-scarlet-knights-logo.png" alt="Rutgers Logo" className="rutgersLogo"></img>Rutgers University, New Brunswick, NJ
-        </span>
-            </li>
-                <li>Full-Stack Coding Bootcamp Certification Dec 2023</li><br></br>
-
-        <li>
-        <span style={{ color: 'red' }}>
-            <img src="/MSU logo.png" alt="Montclair State Logo" className="msuLogo"></img>
-                Montclair State University, Montclair, NJ
-        </span>
+       <ul>
+        < li className="resumeListItem resumeTitle">Education</li>
+        <li className="resumeListItem">
+          <span>
+            <img src="/rutgers-scarlet-knights-logo.png" alt="Rutgers Logo" className="rutgersLogo" />
+            Rutgers University, New Brunswick, NJ
+          </span>
         </li>
-                BS in Environmental Science Jan 2021<br></br><br></br>
-        
-         <li className="resumeTitle">Experience</li>
-            <li>- 6-month Intensive Coding Bootcamp</li><br></br>
-         <li className="resumeTitle">Technical Skills</li>
-            <li>- NodeJS/ReactJS</li>
-            <li>- HTML/CSS/JavaScript</li>
-            <li>- MongoDB</li>
-            <li>- GraphQL</li>
-            <li>- Creating responsive web API's</li>
-            <li>- Client-Server Models </li>
-            <li>- Agile Development</li>
-            <li>- JSON</li>
-            <li>- MERN</li>
-         </h3>
-      </ul>
+        <li className="resumeListItem">Full-Stack Coding Bootcamp Certification Dec 2023</li><br />
 
+        <li className="resumeListItem">
+          <span>
+            <img src="/MSU logo.png" alt="Montclair State Logo" className="msuLogo" />
+            Montclair State University, Montclair, NJ
+          </span>
+        </li>
+        <li className="resumeListItem">BS in Environmental Science Jan 2021</li><br /><br />
+
+        <li className="resumeListItem resumeTitle">Experience</li>
+        <li className="resumeListItem">- 6-month Intensive Coding Bootcamp</li><br />
+        
+        <li className="resumeListItem resumeTitle">Technical Skills</li>
+        <li className="resumeListItem resumeInfo">- NodeJS/ReactJS</li>
+        <li className="resumeListItem resumeInfo">- HTML/CSS/JavaScript</li>
+        <li className="resumeListItem resumeInfo">- MongoDB</li>
+        <li className="resumeListItem resumeInfo">- GraphQL</li>
+        <li className="resumeListItem resumeInfo">- Creating responsive web API's</li>
+        <li className="resumeListItem resumeInfo">- Client-Server Models </li>
+        <li className="resumeListItem resumeInfo">- Agile Development</li>
+        <li className="resumeListItem resumeInfo">- JSON</li>
+        <li className="resumeListItem resumeInfo">- MERN</li>
+      </ul>
     </div>
   );
 };
