a363babb5dfe70724f9a8eaabe6d5f4c34a091fb
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-19 06:46:48 -0400

    changed background for resume page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,47 +1,48 @@
-// Resume.js
 import React from 'react';
 import '../styles/Resume.css';
 
 const Resume = () => {
   return (
-    <div className="resumeDiv">
-      <h2 className="resumeTitle">Resume</h2>
-      {/* Download link */}
-      <a href="/src/components/AnthonyL Resume.docx" className="resumeDownload" download>
-        Download Resume<br /><br />
-      </a>
-       <ul>
-        < li className="resumeListItem resumeTitle">Education</li>
-        <li className="resumeListItem">
-          <span>
-            <img src="/rutgers-scarlet-knights-logo.png" alt="Rutgers Logo" className="rutgersLogo" />
-            Rutgers University, New Brunswick, NJ
-          </span>
-        </li>
-        <li className="resumeListItem">Full-Stack Coding Bootcamp Certification Dec 2023</li><br />
+    <div className="resume-container">
+      <div className="resume-content">
+        <h2 className="resumeTitle">Resume</h2>
+        {/* Download link */}
+        <a href="/src/components/AnthonyL Resume.docx" className="resumeDownload" download>
+          Download Resume
+        </a>
+        <ul>
+          <li className="resumeListItem resumeTitle">Education</li>
+          <li className="resumeListItem">
+            <span>
+              <img src="/rutgers-scarlet-knights-logo.png" alt="Rutgers Logo" className="rutgersLogo" />
+              Rutgers University, New Brunswick, NJ
+            </span>
+          </li>
+          <li className="resumeListItem">Full-Stack Coding Bootcamp Certification Dec 2023</li>
 
-        <li className="resumeListItem">
-          <span>
-            <img src="/MSU logo.png" alt="Montclair State Logo" className="msuLogo" />
-            Montclair State University, Montclair, NJ
-          </span>
-        </li>
-        <li className="resumeListItem">BS in Environmental Science Jan 2021</li><br /><br />
+          <li className="resumeListItem">
+            <span>
+              <img src="/MSU logo.png" alt="Montclair State Logo" className="msuLogo" />
+              Montclair State University, Montclair, NJ
+            </span>
+          </li>
+          <li className="resumeListItem">BS in Environmental Science Jan 2021</li>
 
-        <li className="resumeListItem resumeTitle">Experience</li>
-        <li className="resumeListItem">- 6-month Intensive Coding Bootcamp</li><br />
-        
-        <li className="resumeListItem resumeTitle">Technical Skills</li>
-        <li className="resumeListItem resumeInfo">- NodeJS/ReactJS</li>
-        <li className="resumeListItem resumeInfo">- HTML/CSS/JavaScript</li>
-        <li className="resumeListItem resumeInfo">- MongoDB</li>
-        <li className="resumeListItem resumeInfo">- GraphQL</li>
-        <li className="resumeListItem resumeInfo">- Creating responsive web API's</li>
-        <li className="resumeListItem resumeInfo">- Client-Server Models </li>
-        <li className="resumeListItem resumeInfo">- Agile Development</li>
-        <li className="resumeListItem resumeInfo">- JSON</li>
-        <li className="resumeListItem resumeInfo">- MERN</li>
-      </ul>
+          <li className="resumeListItem resumeTitle">Experience</li>
+          <li className="resumeListItem">- 6-month Intensive Coding Bootcamp</li>
+
+          <li className="resumeListItem resumeTitle">Technical Skills</li>
+          <li className="resumeListItem resumeInfo">- NodeJS/ReactJS</li>
+          <li className="resumeListItem resumeInfo">- HTML/CSS/JavaScript</li>
+          <li className="resumeListItem resumeInfo">- MongoDB</li>
+          <li className="resumeListItem resumeInfo">- GraphQL</li>
+          <li className="resumeListItem resumeInfo">- Creating responsive web APIs</li>
+          <li className="resumeListItem resumeInfo">- Client-Server Models</li>
+          <li className="resumeListItem resumeInfo">- Agile Development</li>
+          <li className="resumeListItem resumeInfo">- JSON</li>
+          <li className="resumeListItem resumeInfo">- MERN</li>
+        </ul>
+      </div>
     </div>
   );
 };
