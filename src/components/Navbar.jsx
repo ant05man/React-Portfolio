40027532698dbfea619ad0720db96a689a6110ad
Anthony Littlejohn <ant05man@yahoo.com>
2025-11-04 13:41:54 -0500

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,13 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import '../styles/Navbar.css';
-
-function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="logo-badge">AL</div>
-      <div className="links-container">
-        <NavLink to="/homepage" className="navbar-link" activeclassname="active">Home</NavLink>
-        <NavLink to="/aboutme" className="navbar-link" activeclassname="active">About Me</NavLink>
-        <NavLink to="/projects" className="navbar-link" activeclassname="active">Projects</NavLink>
-        <NavLink to="/contact" className="navbar-link" activeclassname="active">Contact</NavLink>
-        <NavLink to="/resume" className="navbar-link" activeclassname="active">Resume</NavLink>
-      </div>
-    </nav>
-  );
-}
-
-export default Navbar;
+<nav className="navbar">
+  <div className="navbar-left">
+    <div className="logo-badge">AL</div>
+  </div>
+  
+  <div className="navbar-right">
+    <NavLink to="/homepage" className="navbar-link" activeclassname="active">Home</NavLink>
+    <NavLink to="/aboutme" className="navbar-link" activeclassname="active">About Me</NavLink>
+    <NavLink to="/projects" className="navbar-link" activeclassname="active">Projects</NavLink>
+    <NavLink to="/contact" className="navbar-link" activeclassname="active">Contact</NavLink>
+    <NavLink to="/resume" className="navbar-link" activeclassname="active">Resume</NavLink>
+  </div>
+</nav>
