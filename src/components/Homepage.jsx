8af8a5ac6ac6ebf46e18ade2e5fdba84e997c7a9
Anthony Littlejohn <ant05man@yahoo.com>
2025-11-04 13:08:24 -0500

    changed and added logo to navbar

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,6 +5,7 @@ import '../styles/Homepage.css';
 function Homepage() {
   return (
     <div className="homepage-container">
+      <div className="logo-badge">AL</div>
       <div className="overlay"></div>
       <header className="homepage-content">
         <h1>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,14 @@ import '../styles/Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <NavLink to="/homepage" className="navbar-link" activeclassname="active">Home</NavLink>
-      <NavLink to="/aboutme" className="navbar-link" activeclassname="active">About Me</NavLink>
-      <NavLink to="/projects" className="navbar-link" activeclassname="active">Projects</NavLink>
-      <NavLink to="/contact" className="navbar-link" activeclassname="active">Contact</NavLink>
-      <NavLink to="/resume" className="navbar-link" activeclassname="active">Resume</NavLink>
+      <div className="logo-badge">AL</div>
+      <div className="links-container">
+        <NavLink to="/homepage" className="navbar-link" activeclassname="active">Home</NavLink>
+        <NavLink to="/aboutme" className="navbar-link" activeclassname="active">About Me</NavLink>
+        <NavLink to="/projects" className="navbar-link" activeclassname="active">Projects</NavLink>
+        <NavLink to="/contact" className="navbar-link" activeclassname="active">Contact</NavLink>
+        <NavLink to="/resume" className="navbar-link" activeclassname="active">Resume</NavLink>
+      </div>
     </nav>
   );
 }
